6aaea4599e0c97c9e5716e4a683665d348e7deae
gebhartn <nicholas.gebhart@gmail.com>
2019-09-10 12:42:13 -0700

    Data imports/exports

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { numbers } from "../../../data";
 
 //import any components needed
-// example of import from data.js. Note all the ../   This is how we move through folders. 
+// example of import from data.js. Note all the ../   This is how we move through folders.
 /* 
 import { numbers } from '../../../data' 
 */
