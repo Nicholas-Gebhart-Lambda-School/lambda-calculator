35755c2867472717e922f38d63ad35955bb4fdfa
gebhartn <nicholas.gebhart@gmail.com>
2019-09-11 11:20:17 -0700

    commit and push

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -3,7 +3,7 @@ import React from "react";
 const SpecialButton = props => {
   return (
     <>
-      <button>{props.special}</button>
+      <button className="specialButton">{props.special}</button>
     </>
   );
 };
